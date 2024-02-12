838c1ab2c5b80928c3a00e0b1f68fcee8c8269d3
Hanieh Gharaee <154983983+HaniehGharaee@users.noreply.github.com>
2024-02-12 12:23:58 +0330

    Setting project

---
+++ html.js
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Javascrupt first session</title>
+    </head>
+    <body>
+        <div id="div1"></div>
+        <button onclick='document.getElementById("demo").innerHTML = "Hello"'>click me</button>
+        <script type="text/jsscript">
+            document.getElementById("div1").innerHTML = "session";
+        </script>
+    </body>
+</html>
